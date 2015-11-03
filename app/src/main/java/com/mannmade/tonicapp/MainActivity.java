f08e789273864d557312cceacd5e5b297e1db38a
Edj3 <ejmann@udel.edu>
2015-11-03 00:16:06 -0500

    Layout Adjustments to main activity

--- app/src/main/java/com/mannmade/tonicapp/MainActivity.java
+++ app/src/main/java/com/mannmade/tonicapp/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
 public class MainActivity extends AppCompatActivity {
     ConnectionManager cManager;
     JSONParser jParser;
     String json;
+    ArrayList<LinkedHashMap<String, String>> peopleList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +22,14 @@ public class MainActivity extends AppCompatActivity {
         jParser = new JSONParser();
 
         //Borrowed raw URL from github link
-        json = cManager.connectToURL("https://gist.githubusercontent.com/zorn/6572637c1a1cbb89d4c9/raw/88c80043d4bf6d0feac11de9a575db4573a9b024/people.json");
-        Log.i("Downloading from URL", "JSON Object = " + json);
-        jParser.getJSONforString(json);
+        Thread downloadThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                json = cManager.connectToURL("https://gist.githubusercontent.com/zorn/6572637c1a1cbb89d4c9/raw/88c80043d4bf6d0feac11de9a575db4573a9b024/people.json");
+                Log.i("Downloading from URL", "JSON Object = " + json);
+                peopleList = jParser.getJSONforString(json);
+            }
+        });
+        downloadThread.start();
     }
 }
